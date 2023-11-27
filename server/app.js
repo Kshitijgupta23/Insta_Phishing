30c126bc5bec2e20d3181e46b08ad1f8d29c69dd
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2023-11-27 23:42:08 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-const PORT = 5005
+const PORT = process.env.PORT || 5005;
 
 require("./db/conn");
 const User = require("./model/userSchema");
